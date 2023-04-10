6434c820bbddde409bb7ad49dd1ddedc7a3c8fcb
Khyati Gandhi <125392517+thekhyatigandhi@users.noreply.github.com>
2023-04-10 17:24:46 -0400

    trying to find solutions and fix issues

--- Assets/script.js
+++ Assets/script.js
@@ -7,9 +7,22 @@ let temperature = document.querySelector(
 let wind = document.querySelector(".weather_indicator--wind>.value");
 let humidity = document.querySelector(".weather_indicator--humidity>.value");
 let image = document.querySelector(".weather_image");
+
+let forcast = document.querySelector(".weather_forcast");
+let forcastDay = document.querySelector(".weather_forcast_day");
+let forcastTemp = document.querySelector(".weather_forcast_temp>.value");
+let forcastWind = document.querySelector(".weather_forcast_wind>.value");
+let forcastHumidity = document.querySelector(
+  ".weather_forcast_humidity>.value"
+);
+
 let weatherAPIKey = "754ecc599661b8b35214b8ac0f447f7f"; // generated using openweathermap.org
 let weatherBaseEndPoint =
   "https://api.openweathermap.org/data/2.5/weather?units=metric&appid=" +
+  weatherAPIKey;
+
+let forcastBaseEndPoint =
+  "https://api.openweathermap.org/data/2.5/forecast?units=metric&appid=" +
   weatherAPIKey;
 
 // using units = metrics to convert temp from Fahrenheit to celsius
@@ -23,11 +36,24 @@ let getWeatherByCityName = async (city) => {
 
 getWeatherByCityName("New York");
 
+let getForcastByCityName = async (city) => {
+  let forcastEndpoint = forcastBaseEndPoint + "&q=" + city;
+  let forcastResponse = await fetch(forcastEndpoint);
+  let forcast = await forcastResponse.json();
+
+  return forcast;
+};
+
+getForcastByCityName("New York");
+
 searchInp.addEventListener("keydown", async (e) => {
   if (e.keyCode === 13) {
-    // 13 bcause they keycode for Enter key is 13
+    // 13 because they keycode for Enter key is 13
     let weather = await getWeatherByCityName(searchInp.value);
+
+    let forecast = await getForcastByCityName(searchInp.value);
     updateCurrentWeather(weather);
+    updateWeatherForcast(forecast);
   }
 });
 
@@ -36,7 +62,8 @@ let updateCurrentWeather = (data) => {
   city.textContent = data.name + "," + data.sys.country;
   day.textContent = dayOfWeek();
   humidity.textContent = data.main.humidity;
-  temperature.textContent = data.main.temp;
+  temperature.textContent =
+    data.main.temp > 0 ? "+" + data.main.temp : data.main.temp;
   wind.textContent = data.wind.speed;
 };
 
@@ -47,4 +74,13 @@ let dayOfWeek = () => {
     month: "short",
     day: "numeric",
   });
+};
+
+let updateWeatherForcast = (list) => {
+  console.log(list);
+  day.textContent = dayOfWeek();
+  forcastHumidity.textContent = list.main.humidity;
+  forcastTemp.textContent =
+    list.main.temp > 0 ? "+" + list.main.temp : list.main.temp;
+  forcastWind.textContent = list.wind.speed;
 };
