e909ec2f265ed53edf828c31799d43a09b50b758
Khyati Gandhi <125392517+thekhyatigandhi@users.noreply.github.com>
2023-04-10 14:34:48 -0400

    added api key

--- Assets/script.js
+++ Assets/script.js
@@ -1,20 +1,25 @@
-var searchFormEl = document.querySelector("#search-form");
+let searchInp = document.querySelector(".weather_search");
+let city = document.querySelector(".weather_city");
+let day = document.querySelector(".weather_day");
+let temperature = document.querySelector(
+  ".weather_indicator--temperature>.value"
+);
+let wind = document.querySelector(".weather_indicator--wind>.value");
+let humidity = document.querySelector(".weather_indicator--temperature>.value");
+let image = document.querySelector(".weather_image");
+let weatherAPIKey = "754ecc599661b8b35214b8ac0f447f7f"; // generated using openweathermap.org
+let weatherBaseEndPoint =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&appid=" +
+  weatherAPIKey;
 
-function handleSearchFormSubmit(event) {
-  event.preventDefault();
+// using units = metrics to convert temp from Fahrenheit to celsius
 
-  var searchInputVal = document.querySelector("#search-input").value;
-  var formatInputVal = document.querySelector("#format-input").value;
+let getWeatherByCityName = async (city) => {
+  let endpoint = weatherBaseEndPoint + "&q=" + city;
+  let response = await fetch(endpoint);
+  let weather = await response.json();
 
-  if (!searchInputVal) {
-    console.error("You need a search input value!");
-    return;
-  }
+  console.log(weather);
+};
 
-  var queryString =
-    "./search-results.html?q=" + searchInputVal + "&format=" + formatInputVal;
-
-  location.assign(queryString);
-}
-
-searchFormEl.addEventListener("submit", handleSearchFormSubmit);
+getWeatherByCityName("New York");
