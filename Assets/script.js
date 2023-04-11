583a5a3092ca029ddb8fd826ff9981a2caf5ce70
Khyati Gandhi <125392517+thekhyatigandhi@users.noreply.github.com>
2023-04-11 15:37:15 -0400

    changes in JS

--- Assets/script.js
+++ Assets/script.js
@@ -1,86 +1,72 @@
-let searchInp = document.querySelector(".weather_search");
-let city = document.querySelector(".weather_city");
-let day = document.querySelector(".weather_day");
-let temperature = document.querySelector(
-  ".weather_indicator--temperature>.value"
+// Get DOM elements
+const searchInput = document.querySelector(".weather_search");
+const weatherCity = document.querySelector(".weather_city");
+const weatherDay = document.querySelector(".weather_day");
+const weatherTemp = document.querySelector(
+  ".weather_indicator--temperature .value"
 );
-let wind = document.querySelector(".weather_indicator--wind>.value");
-let humidity = document.querySelector(".weather_indicator--humidity>.value");
-let image = document.querySelector(".weather_image");
-
-let forcast = document.querySelector(".weather_forcast");
-let forcastDay = document.querySelector(".weather_forcast_day");
-let forcastTemp = document.querySelector(".weather_forcast_temp>.value");
-let forcastWind = document.querySelector(".weather_forcast_wind>.value");
-let forcastHumidity = document.querySelector(
-  ".weather_forcast_humidity>.value"
+const weatherWind = document.querySelector(".weather_indicator--wind .value");
+const weatherHumidity = document.querySelector(
+  ".weather_indicator--humidity .value"
 );
+const weatherIcon = document.querySelector(".weather_image");
+const weatherForcastItems = document.querySelectorAll(".weather_forcast_item");
 
-let weatherAPIKey = "754ecc599661b8b35214b8ac0f447f7f"; // generated using openweathermap.org
-let weatherBaseEndPoint =
-  "https://api.openweathermap.org/data/2.5/weather?units=metric&appid=" +
-  weatherAPIKey;
+// OpenWeatherMap API Key
+const apiKey = "754ecc599661b8b35214b8ac0f447f7f";
 
-let forcastBaseEndPoint =
-  "https://api.openweathermap.org/data/2.5/forecast?units=metric&appid=" +
-  weatherAPIKey;
+// Event listener for search input
+searchInput.addEventListener("keypress", async (event) => {
+  if (event.keyCode === 13) {
+    // Check if enter key is pressed
+    const city = searchInput.value;
+    if (city) {
+      // Fetch current weather data
+      const currentWeatherResponse = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+      );
+      const currentWeatherData = await currentWeatherResponse.json();
 
-// using units = metrics to convert temp from Fahrenheit to celsius
+      // Fetch 5-day forecast data
+      const forecastResponse = await fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`
+      );
+      const forecastData = await forecastResponse.json();
 
-let getWeatherByCityName = async (city) => {
-  let endpoint = weatherBaseEndPoint + "&q=" + city;
-  let response = await fetch(endpoint);
-  let weather = await response.json();
-  return weather;
-};
+      // Update DOM with current weather data
+      weatherCity.textContent = currentWeatherData.name;
+      weatherDay.textContent = new Date().toLocaleDateString();
+      weatherTemp.textContent = `${(
+        currentWeatherData.main.temp - 273.15
+      ).toFixed(2)}°C`;
+      weatherWind.textContent = `${currentWeatherData.wind.speed}mph`;
+      weatherHumidity.textContent = `${currentWeatherData.main.humidity}%`;
 
-getWeatherByCityName("New York");
+      // Update DOM with forecast data
+      for (let i = 0; i < weatherForcastItems.length; i++) {
+        const forecastItem = weatherForcastItems[i];
+        const forecast = forecastData.list[i * 8 + 4];
+        const forecastDate = new Date(forecast.dt_txt);
+        const forecastIcon = forecast.weather[0].icon;
+        const forecastTemp = forecast.main.temp;
+        const forecastWind = forecast.wind.speed;
+        const forecastHumidity = forecast.main.humidity;
 
-let getForcastByCityName = async (city) => {
-  let forcastEndpoint = forcastBaseEndPoint + "&q=" + city;
-  let forcastResponse = await fetch(forcastEndpoint);
-  let forcast = await forcastResponse.json();
-
-  return forcast;
-};
-
-getForcastByCityName("New York");
-
-searchInp.addEventListener("keydown", async (e) => {
-  if (e.keyCode === 13) {
-    // 13 because they keycode for Enter key is 13
-    let weather = await getWeatherByCityName(searchInp.value);
-
-    let forecast = await getForcastByCityName(searchInp.value);
-    updateCurrentWeather(weather);
-    updateWeatherForcast(forecast);
+        forecastItem.querySelector(
+          ".weather_forcast_icon"
+        ).src = `https://openweathermap.org/img/w/${forecastIcon}.png`;
+        forecastItem.querySelector(".weather_forcast_day").textContent =
+          forecastDate.toLocaleDateString();
+        forecastItem.querySelector(
+          ".weather_forcast_temp .value"
+        ).textContent = `${(forecastTemp - 273.15).toFixed(2)}°C`;
+        forecastItem.querySelector(
+          ".weather_forcast_wind .value"
+        ).textContent = `${forecastWind}mph`;
+        forecastItem.querySelector(
+          ".weather_forcast_humidity .value"
+        ).textContent = `${forecastHumidity}%`;
+      }
+    }
   }
 });
-
-let updateCurrentWeather = (data) => {
-  console.log(data);
-  city.textContent = data.name + "," + data.sys.country;
-  day.textContent = dayOfWeek();
-  humidity.textContent = data.main.humidity;
-  temperature.textContent =
-    data.main.temp > 0 ? "+" + data.main.temp : data.main.temp;
-  wind.textContent = data.wind.speed;
-};
-
-let dayOfWeek = () => {
-  return new Date().toLocaleDateString("en-EN", {
-    weekday: "long",
-    year: "numeric",
-    month: "short",
-    day: "numeric",
-  });
-};
-
-let updateWeatherForcast = (list) => {
-  console.log(list);
-  day.textContent = dayOfWeek();
-  forcastHumidity.textContent = list.main.humidity;
-  forcastTemp.textContent =
-    list.main.temp > 0 ? "+" + list.main.temp : list.main.temp;
-  forcastWind.textContent = list.wind.speed;
-};
